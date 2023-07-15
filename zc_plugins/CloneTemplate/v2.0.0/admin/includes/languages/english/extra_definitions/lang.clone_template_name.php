6d028764ae912b5cc02b0472a3a181e9459a506d
lat9 <lat9@vinosdefrutastropicales.com>
2023-07-15 12:45:30 -0400

    #6: Refactor language files to use zc158 language arrays

--- zc_plugins/CloneTemplate/v2.0.0/admin/includes/languages/english/extra_definitions/lang.clone_template_name.php
+++ zc_plugins/CloneTemplate/v2.0.0/admin/includes/languages/english/extra_definitions/lang.clone_template_name.php
@@ -1,7 +1,13 @@
 <?php
 // -----
-// Part of the "Clone Template" plugin for Zen Cart v1.5.0 or later
+// Part of the "Clone Template" plugin for Zen Cart v1.5.8 or later, now an encapsulated plugin.
 //
-// Copyright (c) 2016, Vinos de Frutas Tropicales (lat9)
+// Last updated: v2.0.0
 //
-define ('BOX_TOOLS_CLONE_TEMPLATE', 'Clone a Template');
+// Copyright (c) 2016-2023, Vinos de Frutas Tropicales (lat9)
+//
+$define = [
+    'BOX_TOOLS_CLONE_TEMPLATE' => 'Clone a Template',
+];
+
+return $define;
