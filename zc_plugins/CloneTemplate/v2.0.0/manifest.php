7a6e71de06d86facf2dcb4225ea4e3ed8e7a6e55
lat9 <lat9@vinosdefrutastropicales.com>
2023-07-15 11:51:35 -0400

    #6: Add manifest.php

---
+++ zc_plugins/CloneTemplate/v2.0.0/manifest.php
@@ -0,0 +1,17 @@
+<?php
+// -----
+// Part of the "Clone Template" plugin for Zen Cart v1.5.8 or later, v2.0.0.  Now an 'encapsulated' plugin.
+//
+// Copyright (c) 2023, Vinos de Frutas Tropicales (lat9)
+//
+return [
+    'pluginVersion' => 'v2.0.0',
+    'pluginName' => 'Clone a Template',
+    'pluginDescription' => '"Clone" an existing template in your store, copying all the "source" template\'s template-override folders/files and its "Layout Boxes Controller" configuration to a new template.',
+    'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
+    'pluginId' => 2087, // ID from Zen Cart forum
+    'zcVersions' => ['v158'],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => 'https://github.com/lat9/clone_template', // url
+    'pluginGroups' => [],
+];
