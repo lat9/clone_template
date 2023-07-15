87dc583b2163d8d6c1ffe1c7e0601d294ff247ed
lat9 <lat9@vinosdefrutastropicales.com>
2023-07-15 11:41:21 -0400

    #6: Refactor init_includes to ScriptedInstaller

--- zc_plugins/CloneTemplate/v2.0.0/Installer/ScriptedInstaller.php
+++ zc_plugins/CloneTemplate/v2.0.0/Installer/ScriptedInstaller.php
@@ -1,17 +1,30 @@
 <?php
 // -----
-// Part of the "Clone Template" plugin for Zen Cart v1.5.0 or later
+// Part of the "Clone Template" plugin for Zen Cart v1.5.8 or later.  Now an 'encapsulated' plugin.
 //
-// Copyright (c) 2016, Vinos de Frutas Tropicales (lat9)
+// Last updated: v2.0.0
 //
-if (!defined('IS_ADMIN_FLAG')) {
-    die('Illegal Access');
-} 
+// Copyright (c) 2016-2023, Vinos de Frutas Tropicales (lat9)
+//
+use Zencart\PluginSupport\ScriptedInstaller as ScriptedInstallBase;
 
-//----
-// Register the tool into the admin menu structure.
-//
-if (!zen_page_key_exists('toolsCloneTemplate')) {
-    $next_sort = $db->Execute ('SELECT MAX(sort_order) as max_sort FROM ' . TABLE_ADMIN_PAGES . " WHERE menu_key='tools'");
-    zen_register_admin_page ('toolsCloneTemplate', 'BOX_TOOLS_CLONE_TEMPLATE', 'FILENAME_CLONE_TEMPLATE','' ,'tools', 'Y', $next_sort->fields['max_sort'] + 1);
-}    
+class ScriptedInstaller extends ScriptedInstallBase
+{
+    protected function executeInstall()
+    {
+        zen_deregister_admin_pages(['toolsCloneTemplate']);
+        zen_register_admin_page(
+            'toolsCloneTemplate',
+            'BOX_TOOLS_CLONE_TEMPLATE',
+            'FILENAME_CLONE_TEMPLATE',
+            '',
+            'tools',
+            'Y'
+        );
+    }
+
+    protected function executeUninstall()
+    {
+        zen_deregister_admin_pages(['toolsCloneTemplate']);
+    }
+}
